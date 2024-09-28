681c8cf4163cf12bcf31cfe01ee16ea2106075bf
jlerocher <jlerocher@gmail.com>
2024-09-28 02:15:28 +0100

    remove husky and add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const notFound = () => {
+  return (
+    <section className="flex flex-col items-center justify-center w-full mt-64 gap-10">
+      <h1 className="text-4xl font-bold text-red-500">404: Not Found</h1>
+      <p className="">Page not found. Return to <Link href="/" className="underline underline-offset-4">the homepage</Link></p>
+    </section>
+  )
+}
+
+export default notFound
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-
 export default function Home() {
     return (
         <section className="w-full">
-            <p className="text-center mt-60">Start editing the <strong>`src/app/page.tsx`</strong> file to see this page update in real-time.</p>
+            <p className="text-center mt-60">
+                Start editing the <strong>`src/app/page.tsx`</strong> file to
+                see this page update in real-time.
+            </p>
         </section>
     );
 }
