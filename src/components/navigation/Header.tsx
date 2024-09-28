517e262e103cfadde79de25922d06bc1a7b5c983
jlerocher <jlerocher@gmail.com>
2024-09-28 01:37:36 +0100

    First commit

---
+++ src/components/navigation/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { ModeToggle } from "../ui/mode-toggle";
+
+const Header = () => {
+    return (
+        <header className="flex items-center justify-between py-4 px-4">
+            <h1><Link href="/">Next.js 14 Starter</Link></h1>
+            <ModeToggle />
+        </header>
+    );
+};
+
+export default Header;
