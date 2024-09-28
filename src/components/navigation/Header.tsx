f2a53a27fb89e9c4005999fb944c4bf268dc8dc8
jlerocher <jlerocher@gmail.com>
2024-09-28 01:38:48 +0100

    Modify precommit hook

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -4,7 +4,9 @@ import { ModeToggle } from "../ui/mode-toggle";
 const Header = () => {
     return (
         <header className="flex items-center justify-between py-4 px-4">
-            <h1><Link href="/">Next.js 14 Starter</Link></h1>
+            <h1>
+                <Link href="/">Next.js 14 Starter</Link>
+            </h1>
             <ModeToggle />
         </header>
     );
