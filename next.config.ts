9aef0efc5495d02530a97e1799cf3cb2e2a82f42
jlerocher <jlerocher@gmail.com>
2024-11-08 17:34:25 +0100

    Modify nextconfig and prisma client tw plugins

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const withPWA = require("@ducanh2912/next-pwa").default({
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    dest: "public",
-    fallbacks: {
-        //image: "/static/images/fallback.png",
-        document: "/offline", // if you want to fallback to a custom page rather than /_offline
-        // font: '/static/font/fallback.woff2',
-        // audio: ...,
-        // video: ...,
-    },
-    workboxOptions: {
-        disableDevLogs: true,
-    },
-    // ... other options you like
-});
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {
+    /* config options here */
+};
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
--- src/lib/prisma/prismadb.ts
+++ src/lib/prisma/prismadb.ts
@@ -1,12 +1,5 @@
-"use server";
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined;
-}
-
-const prisma = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
