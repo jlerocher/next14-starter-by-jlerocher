aa4b5739973e491d02a51e12c06bff4f423ffa11
jlerocher <jlerocher@gmail.com>
2024-11-08 21:21:56 +0100

    Remove NextAuth implementation and related components
    This commit removes all code and configurations related to NextAuth, including authentication components, providers, hooks, and routes. It simplifies the project structure by focusing on other functionalities while removing unused code.

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,19 +1,14 @@
-import OneTapComponent from "@/components/onetap";
-import NextAuthProvider from "./next-auth-provider";
 import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <NextAuthProvider>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-            >
-                {children}
-                <OneTapComponent googleClientID={process.env.AUTH_GOOGLE_ID!} />
-            </ThemeProvider>
-        </NextAuthProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
     );
 }
